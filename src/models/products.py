d57859b05867861556810feb093d6bb9605dfcb7
a18091986 <a18091986@gmail.com>
2026-04-22 17:50:13 +0300

    models selflinks relations

--- src/models/products.py
+++ src/models/products.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
-from sqlalchemy import Integer, String, Numeric, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, Integer, String, Numeric, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.database import Base
 
@@ -11,16 +11,27 @@ class Product(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     description: Mapped[str | None] = mapped_column(String(500), nullable=True)
-    price: Mapped[Decimal] = mapped_column(Numeric(10,2), nullable=False)
-    img_url: Mapped[str] = mapped_column(String(200), nullable=True)
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+    img_url: Mapped[str | None] = mapped_column(String(200), nullable=True)
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("categories.id"), 
+        nullable=False
+        )
+
+    category: Mapped["Category"] = relationship(
+        "Category", 
+        back_populates="products",
+        lazy="joined",
+        uselist=False
+    )
 
 
-# if __name__ == "__main__":
-#     from sqlalchemy.schema import CreateTable
-#     from src.models.categories import Category
-#     print(CreateTable(Category.__table__))
-#     print(CreateTable(Product.__table__))
+if __name__ == "__main__":
+    from sqlalchemy.schema import CreateTable
+    from src.models.categories import Category
+    print(CreateTable(Category.__table__))
+    print(CreateTable(Product.__table__))
 
 
